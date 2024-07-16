223355737308cff9db3b390e73fe74a4ef490e1e
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-16 14:23:41 +0300

    variables

---
+++ variables/variables.py
@@ -0,0 +1,10 @@
+# variables in python
+print('I enjoy python')
+name = 'Godfrey' #string
+print('Hello ' +name)
+num = 3 # number
+print(num)
+height = 34.6 #float
+print(height)
+human = True #bool
+
