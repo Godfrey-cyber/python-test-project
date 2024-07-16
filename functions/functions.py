65ca8bc7dc222a577ebb9f9867daa22b459ba3cc
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-16 18:34:48 +0300

    functions.py

---
+++ functions/functions.py
@@ -0,0 +1,15 @@
+# function - a block of code that does something when called
+
+
+
+# def hello():
+    # print("Your full name")
+    # return l_name
+
+# hello()
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+multiple = multiply(3, 7)
+print(multiple)
