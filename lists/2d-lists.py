cd7bd2ad940d7e83d9a8bdcfe17a02c16a800778
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-16 18:46:35 +0300

    2d-lists.py

---
+++ lists/2d-lists.py
@@ -0,0 +1,7 @@
+# 2D Lists -  a list of lists
+drinks = ['coke', 'fanta', 'tea']
+dinner = ['pizza', 'sushi', 'tacco']
+dessert = ['cake', 'ice-cream']
+
+food = [drinks, dinner, dessert]
+print(food)
