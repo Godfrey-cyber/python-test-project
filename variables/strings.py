651174dda645e1e832678747d9c9d30d258e96f8
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-16 14:44:21 +0300

    type_casting

---
+++ variables/strings.py
@@ -0,0 +1,12 @@
+# Strings
+name = "Godfrey Ndiritu"
+print(len(name)) #lenght
+print(name.find('r')) # position of r
+print(name.upper()) #uppercase
+print(name.lower()) # lowercase
+print(name.capitalize()) # capitalize
+print(name.isdigit()) #if is number
+print(name.isalpha()) # contains only alpjhabets letters
+print(name.count('o')) # counts all o
+print(name.replace('o', 'a')) # replace o with a
+print(name*3) #*3
---
+++ variables/type_casting.py
@@ -0,0 +1,12 @@
+#Type castint
+x = 2 #int
+age = 12.5 #float
+human = False #bool
+height = "5" #string
+
+age = int(age) #integer
+height = int(height) #integer
+x = float(x) # floating
+print(age)
+print(x)
+print(height)
