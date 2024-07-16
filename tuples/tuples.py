d02bc4b0ea564e5e1a1aec0f300092f51f7c6b4d
Godfrey-cyber <godfreyndiritu26@gmail.com>
2024-07-16 18:38:22 +0300

    tuples

---
+++ tuples/tuples.py
@@ -0,0 +1,8 @@
+# tuples - a collection that is ordered and unchaneable - grouping together related products
+
+student = ('Godfrey', 21, "male")
+for x in student:
+    print(x)
+
+if "Godfrey" in student:
+    print('Godfrey is here')
